a43fb0943dcb9d600238558de86a2b2542b42487
Anna Karutina <anna.karutina@khk.ee>
2023-09-19 13:52:55 +0300

    data types and conversions

---
+++ datatypes.js
@@ -0,0 +1,33 @@
+// data types
+// String
+let firstname = 'Anna'
+let lastname = 'Karutina'
+
+console.log(firstname + ' ' + lastname)
+console.log(typeof firstname)
+
+// Numbers
+let goodMark = 5
+console.log(typeof goodMark)
+
+let percent = 50.3
+console.log(typeof percent)
+
+//Boolean
+let isToday = true
+console.log(typeof isToday)
+
+// type conversions
+// number -> String
+console.log(typeof String(555))
+console.log(String(5+5))
+console.log(String(5.5) + String(5.5))
+
+// boolean -> String
+console.log(String(true))
+
+// String -> Number
+console.log(Number('666'))
+console.log(Number('66.6'))
+console.log(parseInt(100.8))
+console.log(parseFloat(100.3))
